de28d591764e2c373c7183c8e66e108779d65c98
AdrianFernandoMorales <159586790+AdrianFernandoMorales@users.noreply.github.com>
2024-03-21 21:50:39 -0600

    Add files via upload

---
+++ EVA2_8_VOLADO/src/eva2_8_volado/EVA2_8_VOLADO.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_8_volado;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_8_VOLADO extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+        //Numero aleatoria en java
+        //Valores entre 0 y 1
+        //0.3234235313212
+        double Moneda;
+        Moneda = Math.random();
+        System.out.println("El valor de la moneda = " + Moneda);
+        
+        //0.5 es la mitad del rango
+        if (Moneda > 0.5)
+        System.out.println("AGUILA");
+        else
+        System.out.println("SELLO");
+    }
+    
+}
