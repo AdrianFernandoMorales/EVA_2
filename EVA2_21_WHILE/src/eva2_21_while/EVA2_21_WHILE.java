77896ac49ff4d436fe09c071d5a18352ef885296
AdrianFernandoMorales <159586790+AdrianFernandoMorales@users.noreply.github.com>
2024-04-18 00:33:09 -0600

    Add files via upload

---
+++ EVA2_21_WHILE/src/eva2_21_while/EVA2_21_WHILE.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_21_while;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_21_WHILE extends Application {
+   
+   @Override
+   public void start(Stage primaryStage) {
+      Button btn = new Button();
+      btn.setText("Say 'Hello World'");
+      btn.setOnAction(new EventHandler<ActionEvent>() {
+         
+         @Override
+         public void handle(ActionEvent event) {
+            System.out.println("Hello World!");
+         }
+      });
+      
+      StackPane root = new StackPane();
+      root.getChildren().add(btn);
+      
+      Scene scene = new Scene(root, 300, 250);
+      
+      primaryStage.setTitle("Hello World!");
+      primaryStage.setScene(scene);
+      primaryStage.show();
+   }
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      launch(args);
+      //FOR --> CUENTA 
+      //WHILE Y DO WHILE --> CON CONDICIONES
+      
+     int cont = 11;
+     while(cont <=10){
+        System.out.print(cont + " - ");
+        cont++;
+   }
+   }
+}
